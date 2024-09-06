5617070b9a2b5c04dd561108f467cb2b7e72d6b0
jeongeungyeong <jeongeung1124@gmail.com>
2024-09-06 20:44:10 +0900

    docs : 프로젝트 초기 설정

---
+++ src/main/java/org/example/goldroomresource/GoldRoomResourceApplication.java
@@ -0,0 +1,13 @@
+package org.example.goldroomresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldRoomResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldRoomResourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/goldroomresource/GoldRoomResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.goldroomresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldRoomResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
