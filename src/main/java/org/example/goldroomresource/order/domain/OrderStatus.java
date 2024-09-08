3c1fefc4d1b6e774ce10fc5495e50b5254ec6543
jeongeungyeong <jeongeung1124@gmail.com>
2024-09-08 22:23:27 +0900

    refactor : 구매자, 판매자 상태 변경

--- src/main/java/org/example/goldroomresource/order/domain/OrderStatus.java
+++ src/main/java/org/example/goldroomresource/order/domain/OrderStatus.java
@@ -1,10 +1,12 @@
 package org.example.goldroomresource.order.domain;
 
 public enum OrderStatus {
-    BUY_ORDER_COMPLETED("주문 완료"),
+    // 판매자
+    SELL_ORDER_COMPLETED("주문 완료"),
     DEPOSIT_COMPLETED("입금 완료"),
     SHIPMENT_COMPLETED("발송 완료"),
-    SELL_ORDER_COMPLETED("주문 완료"),
+    // 구매자
+    BUY_ORDER_COMPLETED("주문 완료"),
     TRANSFER_COMPLETED("송금 완료"),
     RECEIPT_COMPLETED("수령 완료");
 
