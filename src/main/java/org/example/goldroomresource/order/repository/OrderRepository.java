156a1b7228e7a942439b9a147d93fd0f0b712b0d
jeongeungyeong <jeongeung1124@gmail.com>
2024-09-08 18:03:04 +0900

    chore : 사용하지 않는 import문 삭제

--- src/main/java/org/example/goldroomresource/order/repository/OrderRepository.java
+++ src/main/java/org/example/goldroomresource/order/repository/OrderRepository.java
@@ -3,7 +3,5 @@ package org.example.goldroomresource.order.repository;
 import org.example.goldroomresource.order.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
